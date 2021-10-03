5dc179175cda2238465b0b58a2f21effd7c7df79
Benjamin Vaughn <benjaminvaughn91@gmail.com>
2021-10-03 20:07:47 +0200

    combined states into movie/review obj

--- src/components/reviewBox/ReviewBox.js
+++ src/components/reviewBox/ReviewBox.js
@@ -1,22 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import movieService from '../../services/movies'
-import defaultPosterPath from '../../static/img/notfound_poster.png'
 import { useHistory } from "react-router-dom"
 
 const ReviewBox = (props) => {
     const {review} = props;
 
-    const [title, setTitle] = useState('')
-    const [imgUrl, setImgUrl] = useState('')
+    const [movie, setMovie] = useState({})
 
     useEffect(() => {
       let isMounted = true
-      movieService.getMovie(review.movieId).then(movie => {
-        if (isMounted) {
-          setTitle(movie.title)
-          movie.poster_path ? setImgUrl(movie.poster_path)
-            : setImgUrl(defaultPosterPath)
-        }
+      movieService.getMovie(review.movieId).then(movieObj => {
+        if (isMounted) 
+          setMovie(movieObj)
       })  
       return () => { isMounted = false }
     }, [review.movieId])
@@ -33,10 +28,10 @@ const ReviewBox = (props) => {
       <button onClick={goToReview}>
       <div className="flex flex-nowrap h-48 rounded overflow-hidden border-2 border-transparent hover:border-gray-100 shadow-2xl hover:shadow-2xl hover:shadow-2xl hover:shadow-inner">
           
-          <img className="pt-0 w-20 h-20 lg:w-auto lg:h-auto hidden sm:block" src={imgUrl} alt={title}></img>
+          <img className="pt-0 w-20 h-20 lg:w-auto lg:h-auto hidden sm:block" src={movie.poster_path} alt={movie.title}></img>
           
           <div className="px-6 py-0 mx-auto  ">
-              <p className="font-bold w-full overflow-ellipsis text-xl ">{title}</p>
+              <p className="font-bold w-full overflow-ellipsis text-xl ">{movie.title}</p>
               <p className="font-bold w-full overflow-clip text-blue-100 text-md mb-2">{review.author}</p>
               <p className=" w-full overflow-ellipsis p-0 text-gray-700 text-base">{review.content}</p>
           </div>
